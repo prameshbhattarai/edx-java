ad24d02e4a9e8bc8af58b95da501dbc0da7929e6
pramesh bhattarai <pra4mesh@gmail.com>
2018-05-14 18:58:33 +0545

    Remove duplicate form list.

---
+++ src/LinkedList/list/removeDuplicateFromList.java
@@ -0,0 +1,44 @@
+package LinkedList.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class removeDuplicateFromList {
+
+    private static void printList(List<Integer> mine){
+        for(Integer s : mine)
+            System.out.print(s+" ");
+        System.out.println();
+    }
+    private static List<Integer> createRandomList(int size, int max){
+        List<Integer> result = new ArrayList<>();
+
+        for (int i=0; i< size; i++){
+            result.add( (int)( Math.random()*max));
+        }
+        return result;
+    }
+    /*
+     * You should implement this function without resorting to use HashSet
+     * and maintaining the order of the List
+     */
+    private static void removeDuplicates(List<Integer> mine){
+        List<Integer> setOfList = new ArrayList<>();
+        for (int i = 0; i < mine.size()-1; i++) {
+            if(setOfList.indexOf(mine.get(i)) < 0) {
+                setOfList.add(mine.get(i));
+            }
+        }
+        mine.clear();
+        mine.addAll(setOfList);
+    }
+
+    public static void main(String args[]){
+        List<Integer> myList = createRandomList(10,5);
+        printList(myList);
+        //remove Duplicates from myList
+        removeDuplicates(myList);
+        printList(myList);
+    }
+}
